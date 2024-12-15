028bb9cb0d0791b3bc80c39fddc4a2ee9f4ecfdf
skolathu-cds <sriramk.cds@gmail.com>
2024-12-15 09:43:22 +0530

    punkt_tab download

--- test1.py
+++ test1.py
@@ -2,6 +2,8 @@ import os
 from nltk.tokenize import sent_tokenize
 import nltk
 nltk.download('stopwords')
+nltk.download('punkt_tab')
+
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
