cf0ac1da22e81b4f01c8e54bd452633279fc95fd
skolathu-cds <sriramk.cds@gmail.com>
2024-12-15 09:40:16 +0530

    nltkstopwords added

--- test1.py
+++ test1.py
@@ -1,5 +1,7 @@
 import os
 from nltk.tokenize import sent_tokenize
+import nltk
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
